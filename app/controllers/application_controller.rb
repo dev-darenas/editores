17e9a8a26d56ad345f005ec63b264381abea662c
Jorge Quiñones <jorge2787@gmail.com>
2019-02-14 17:08:17 -0500

    devise views styled added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  layout :layout_by_resource
+  # before_action :authenticate
+
+
+  def after_sign_in_path_for(resource)
+      # if current_user.role == 'admin' || current_user.role == 'store_admin'
+      #   admin_index_path
+      # else
+      #   root_path
+      # end
+  end
+
+
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
 end
