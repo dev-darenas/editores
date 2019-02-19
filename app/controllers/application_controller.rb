a9dfd93f937ed86454beb79c3fe8f2a7b2c29ca8
Jorge Quiñones <jorge2787@gmail.com>
2019-02-19 16:16:41 -0500

    protect sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
+  protect_from_forgery prepend: true
   layout :layout_by_resource
   # before_action :authenticate
 
