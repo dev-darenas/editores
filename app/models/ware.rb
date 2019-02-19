e5e17533d0154fa01aa20b9e4f2680f8d0d184f9
Jorge Quiñones <jorge2787@gmail.com>
2019-02-19 14:53:20 -0500

    wares and load_wares views

--- app/models/ware.rb
+++ app/models/ware.rb
@@ -1,2 +1,7 @@
 class Ware < ApplicationRecord
+
+  validates :reference, presence: true
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
 end
